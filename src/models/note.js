245ebd5ad031ddf2383088c84a56a409092a32b9
Artem Shevchenko <tema.ua1999@gmail.com>
2026-09-20 16:12:19 +0200

    adding auth

--- src/models/note.js
+++ src/models/note.js
@@ -11,12 +11,17 @@ const noteSchema = new Schema(
     content: {
       type: String,
       trim: true,
-      default: "",
+      default: '',
     },
     tag: {
       type: String,
       default: 'Todo',
       enum: TAGS,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
     },
   },
   { timestamps: true },
@@ -24,6 +29,7 @@ const noteSchema = new Schema(
 
 noteSchema.index({
   tag: 1,
+  userId: 1,
 });
 
 export const Note = model('Note', noteSchema);
